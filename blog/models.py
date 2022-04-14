9785adba0d463749293d87649f250a14fd5a6aa7
Sincere-Dhairya <varshneydhairya4@gmail.com>
2022-04-14 23:40:11 +0530

    14apr22

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 from django.urls import reverse
 from PIL import Image
+from users.models import author
+
 
 class Post(models.Model):
+    id = models.BigAutoField(primary_key=True, null=False)
+    author_id = models.ForeignKey(to= author, on_delete=models.CASCADE, default= 1)
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User,on_delete=models.CASCADE) #if you delete the user it will delete all the post (CASCADE)
     header_image = models.ImageField(null=True , blank=True,upload_to="images/")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail',kwargs={'pk':self.pk})    
+        return reverse('post-detail',kwargs={'id':self.id})    
 
     
 class Comment(models.Model):
-    post = models.ForeignKey(Post,related_name="comments",on_delete=models.CASCADE)   
+    id = models.BigAutoField(primary_key=True, null=False)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)   
     name = models.CharField(max_length=255)
     body = models.TextField()
     date_added = models.DateTimeField(default=timezone.now)
